18e652d4175f0195fdcca87b1a64107003fea015
漆伟龙 <qiwl@maycur.com>
2024-06-25 10:03:23 +0800

    feat: 加入测试代码

--- src/pages/menu-detail/index.tsx
+++ src/pages/menu-detail/index.tsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+
+const prefix = "menu-detail-wrapper";
+interface menuInterface {
+  label: string;
+  value: string;
+}
+
+interface listFace {
+  [key: string]: menuInterface[];
+}
+
+const soupList: menuInterface[] = [
+  {
+    label: "冬瓜排骨汤",
+    value: "冬瓜加排骨"
+  }
+];
+const vegetableList: menuInterface[] = [];
+const meatList: menuInterface[] = [];
+
+const menuList: listFace = {
+  vegetable: vegetableList,
+  meat: meatList,
+  soup: soupList
+};
 
 function MenuDetail() {
   const location = useLocation();
   const params = useParams();
+  const [currentMenuList, setCurrentMenuList] = useState<menuInterface[]>([]);
+
+  const { id } = params;
+
+  useEffect(() => {
+    if (id) {
+      const arr = menuList[id];
+      setCurrentMenuList(arr);
+    }
+  }, [id]);
+
   console.log(params);
   console.log(location);
-  return <div>11111</div>;
+  console.log(currentMenuList, "currentMenuList");
+  return <div className={prefix}>11111</div>;
 }
 
 export default MenuDetail;
